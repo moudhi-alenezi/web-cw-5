aa8edc7bcacf36f4414ef0cffa89199c142a7ae9
Moudhi Alenezi <therealmoudhi@gmail.com>
2022-03-16 18:54:44 +0300

    CW2D5

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,21 @@
-alert ("%c moudhi")
+alert ("moudhi")
 console.log(18)
-console.log("%c im a grad", "color: red")
+console.log("%cim a grad", "color: red")
 // javaScript is not java //
+let budget =  20 
+console.log("budget is " + budget)
+ if (budget >= 30 && budget <= 80) {
+        console.log("you can buy the gift")
+    } else {
+        console.log("you can not buy the gift")
+    }    
+
+let total = budget + 30
+console.log("budget is after adding 30 : " + total)
+
+if (total >= 30 && total <= 80) {
+    console.log("you can buy the gift")
+} else {
+    console.log("you can not buy the gift")
+} 
+
